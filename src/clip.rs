ee23f24be5bc5d025c8d1bb7e644987b6ddb21d1
Éverton M. Vieira <everton.muvi@gmail.com>
2022-05-06 15:24:39 -0300

    prototype development

--- src/clip.rs
+++ src/clip.rs
@@ -1,9 +1,18 @@
-use clap::{ArgMatches, Command};
+use clap::{Arg, ArgMatches, Command};
 
 pub fn parse() -> ArgMatches {
     Command::new("ichain")
         .version(clap::crate_version!())
         .about("IChain is a command program that chains muliple executions of programs passing inputs and outputs as configured in a INI like file.")
         .author("Éverton M. Vieira <everton.muvi@gmail.com>")
+        .arg(
+            Arg::new("input")
+                .short('i')
+                .long("input")
+                .value_name("INI")
+                .takes_value(true)
+                .required(true)
+                .help("Chain the programs configured in INI file."),
+        )
         .get_matches()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 mod clip;
 
 fn main() {
-    let _ = clip::parse();
+    let args = clip::parse();
+    let input = args
+        .value_of("input")
+        .expect("You have to pass an input argument.");
+    let contents =
+        std::fs::read_to_string(input).expect("Something went wrong reading the input file.");
+    println!("{}", contents);
 }
