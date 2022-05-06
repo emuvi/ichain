dcda4e0eaca1948901b5cef8f66b06f56de5f1d5
Éverton M. Vieira <everton.muvi@gmail.com>
2022-05-06 17:18:31 -0300

    prototype development

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,46 @@ fn main() {
         .expect("You have to pass an input argument.");
     let contents =
         std::fs::read_to_string(input).expect("Something went wrong reading the input file.");
-    println!("{}", contents);
+    let mut setups: Vec<Setup> = Vec::new();
+    let mut block: Vec<&str> = Vec::new();
+    contents.lines().for_each(|line| {
+        let clean = line.trim();
+        if !clean.is_empty() {
+            if clean.starts_with("[") {
+                if !block.is_empty() {
+                    setups.push(Setup::new(&block));
+                    block.clear();
+                }
+            }
+            block.push(clean);
+        }
+    });
+    if !block.is_empty() {
+        setups.push(Setup::new(&block));
+        block.clear();
+    }
+    setups.iter().for_each(|setup| {
+        println!("{:?}", setup);
+    });
+}
+
+#[derive(Debug)]
+struct Setup {
+    pub name: String,
+    pub args: Vec<String>,
+}
+
+impl Setup {
+    pub fn new(block: &Vec<&str>) -> Self {
+        let mut name = String::new();
+        let mut args = Vec::new();
+        block.iter().for_each(|line| {
+            if line.starts_with("[") {
+                name.push_str(line.trim_start_matches("[").trim_end_matches("]"));
+            } else {
+                args.push(String::from(*line));
+            }
+        });
+        Self { name, args }
+    }
 }
