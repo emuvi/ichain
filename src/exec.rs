d36e743b0a17c42c98544705181e66e4f28ed499
Éverton M. Vieira <everton.muvi@gmail.com>
2022-05-08 15:48:56 -0300

    prototype development

--- src/exec.rs
+++ src/exec.rs
@@ -1,5 +1,208 @@
-use super::Chained;
+use std::io::{BufRead, BufReader};
+use std::process::{Command, Stdio};
+use std::sync::{Arc, RwLock};
+use std::thread::{self, JoinHandle};
+
+use crate::setup::Chained;
+use crate::setup::PassOn;
+use crate::setup::PassTo;
 
 pub fn start(chaineds: Vec<Chained>) {
-    println!("Started: {:?}", chaineds);
+    let results: Results = Arc::new(RwLock::new(Vec::new()));
+    let mut handles: Vec<JoinHandle<()>> = Vec::new();
+    for chained in chaineds {
+        let results = results.clone();
+        handles.push(thread::spawn(move || execute(chained, results)));
+    }
+    for handle in handles {
+        handle.join().unwrap();
+    }
+}
+
+type Results = Arc<RwLock<Vec<Arc<RwLock<Process>>>>>;
+
+struct Process {
+    name: String,
+    errs: Vec<String>,
+    outs: Vec<String>,
+    done: bool,
+}
+
+fn execute(chained: Chained, results: Results) {
+    let process = Arc::new(RwLock::new(Process {
+        name: chained.name.clone(),
+        errs: Vec::new(),
+        outs: Vec::new(),
+        done: false,
+    }));
+    results.write().unwrap().push(process.clone());
+    let mut command = Command::new(chained.name.clone());
+    chained.ways.iter().for_each(|(to, on)| {
+        if to == &PassTo::Param {
+            match on {
+                &PassOn::DirectLike(ref value) => {
+                    command.arg(value);
+                }
+                &PassOn::ExpectAllErrOf(ref name) => {
+                    command.arg(get_all_err_of_lined(name, results.clone()));
+                }
+                &PassOn::ExpectEachErrOf(ref name) => {
+                    for argument in get_all_err_of_vected(name, results.clone()) {
+                        command.arg(argument);
+                    }
+                }
+                &PassOn::ExpectNthErrOf(ref nth, ref of) => {
+                    command.arg(get_nth_err_of(nth, of, results.clone()));
+                }
+                &PassOn::ExpectAllOutOf(ref name) => {
+                    command.arg(get_all_out_of_lined(name, results.clone()));
+                }
+                &PassOn::ExpectEachOutOf(ref name) => {
+                    for argument in get_all_out_of_vected(name, results.clone()) {
+                        command.arg(argument);
+                    }
+                }
+                &PassOn::ExpectNthOutOf(ref nth, ref of) => {
+                    command.arg(get_nth_out_of(nth, of, results.clone()));
+                }
+            }
+        }
+    });
+
+    let child = command
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    let read_err = {
+        let stderr = child.stderr.unwrap();
+        let process = process.clone();
+        thread::spawn(move || {
+            let mut reader = BufReader::new(stderr);
+            let mut err_line = String::new();
+            loop {
+                err_line.clear();
+                let size = reader.read_line(&mut err_line).unwrap();
+                if size == 0 {
+                    break;
+                } else {
+                    process.write().unwrap().errs.push(err_line.clone());
+                }
+            }
+        })
+    };
+
+    let read_out = {
+        let stdout = child.stdout.unwrap();
+        let process = process.clone();
+        thread::spawn(move || {
+            let mut reader = BufReader::new(stdout);
+            let mut out_line = String::new();
+            loop {
+                out_line.clear();
+                let size = reader.read_line(&mut out_line).unwrap();
+                if size == 0 {
+                    break;
+                } else {
+                    process.write().unwrap().outs.push(out_line.clone());
+                }
+            }
+        })
+    };
+
+    read_err.join().unwrap();
+    read_out.join().unwrap();
+    process.write().unwrap().done = true;
+}
+
+fn get_all_err_of_lined(name: &str, results: Results) -> String {
+    loop {
+        for process in results.read().unwrap().iter() {
+            let reader = process.read().unwrap();
+            if reader.name == name {
+                if reader.done {
+                    return reader.errs.join(" ");
+                }
+            }
+        }
+        thread::sleep(std::time::Duration::from_millis(10));
+    }
+}
+
+fn get_all_err_of_vected(name: &str, results: Results) -> Vec<String> {
+    loop {
+        for process in results.read().unwrap().iter() {
+            let reader = process.read().unwrap();
+            if reader.name == name {
+                if reader.done {
+                    return reader.errs.clone();
+                }
+            }
+        }
+        thread::sleep(std::time::Duration::from_millis(10));
+    }
+}
+
+fn get_nth_err_of(nth: &usize, name: &str, results: Results) -> String {
+    loop {
+        for process in results.read().unwrap().iter() {
+            let reader = process.read().unwrap();
+            if reader.name == name {
+                if reader.errs.len() > *nth {
+                    return reader.errs[*nth].clone();
+                }
+                if reader.done {
+                    panic!(format!("Nth Err {} of {} will never come.", nth, name));
+                }
+            }
+        }
+        thread::sleep(std::time::Duration::from_millis(10));
+    }
+}
+
+fn get_all_out_of_lined(name: &str, results: Results) -> String {
+    loop {
+        for process in results.read().unwrap().iter() {
+            let reader = process.read().unwrap();
+            if reader.name == name {
+                if reader.done {
+                    return reader.outs.join(" ");
+                }
+            }
+        }
+        thread::sleep(std::time::Duration::from_millis(10));
+    }
+}
+
+fn get_all_out_of_vected(name: &str, results: Results) -> Vec<String> {
+    loop {
+        for process in results.read().unwrap().iter() {
+            let reader = process.read().unwrap();
+            if reader.name == name {
+                if reader.done {
+                    return reader.outs.clone();
+                }
+            }
+        }
+        thread::sleep(std::time::Duration::from_millis(10));
+    }
+}
+
+fn get_nth_out_of(nth: &usize, name: &str, results: Results) -> String {
+    loop {
+        for process in results.read().unwrap().iter() {
+            let reader = process.read().unwrap();
+            if reader.name == name {
+                if reader.outs.len() > *nth {
+                    return reader.outs[*nth].clone();
+                }
+                if reader.done {
+                    panic!(format!("Nth Out {} of {} will never come.", nth, name));
+                }
+            }
+        }
+        thread::sleep(std::time::Duration::from_millis(10));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod clip;
-
 mod exec;
+mod setup;
+
+use setup::Chained;
 
 fn main() {
     let args = clip::parse();
@@ -35,113 +37,4 @@ fn main() {
         });
     }
     exec::start(chaineds);
-}
-
-#[derive(Debug)]
-pub struct Chained {
-    pub name: String,
-    pub ways: Vec<(PassTo, PassOn)>,
-}
-
-impl Chained {
-    pub fn new(block: &Vec<&str>) -> Self {
-        let mut name = String::new();
-        let mut ways = Vec::new();
-        block.iter().for_each(|line| {
-            if line.starts_with("[") {
-                name.push_str(line.trim_start_matches("[").trim_end_matches("]"));
-            } else if line.starts_with(">") {
-                let line = &line[1..].trim();
-                for on in get_ways_on(line) {
-                    ways.push((PassTo::Param, on));
-                }
-            } else if line.starts_with("|") {
-                let line = &line[1..].trim();
-                for on in get_ways_on(line) {
-                    ways.push((PassTo::Input, on));
-                }
-            }
-        });
-        Chained { name, ways }
-    }
-}
-
-#[derive(Debug)]
-pub enum PassTo {
-    Param,
-    Input,
-}
-
-#[derive(Debug)]
-pub enum PassOn {
-    DirectLike(String),
-    ExpectAllOf(String),
-    ExpectEachOf(String),
-    ExpectNthOf(u32, String),
-}
-
-fn get_ways_on(of_setup_line: &str) -> Vec<PassOn> {
-    let mut results = Vec::new();
-    let mut making_actual = String::new();
-    let mut inside_quotes = false;
-    let mut escaped_char = false;
-    for actual_char in of_setup_line.chars() {
-        if inside_quotes {
-            if escaped_char {
-                if actual_char == 'n' {
-                    making_actual.push('\n');
-                } else if actual_char == 'r' {
-                    making_actual.push('\n');
-                } else if actual_char == 't' {
-                    making_actual.push('\t');
-                } else {
-                    making_actual.push(actual_char)
-                }
-                escaped_char = false;
-            } else if actual_char == '\\' {
-                escaped_char = true;
-            } else if actual_char == '"' {
-                try_push_new_pass_on(&making_actual, &mut results);
-                making_actual.clear();
-            } else {
-                making_actual.push(actual_char);
-            }
-        } else {
-            if actual_char == '"' {
-                inside_quotes = true;
-            } else if actual_char == ' ' {
-                try_push_new_pass_on(&making_actual, &mut results);
-                making_actual.clear();
-            } else {
-                making_actual.push(actual_char);
-            }
-        }
-    }
-    try_push_new_pass_on(&making_actual, &mut results);
-    results
-}
-
-fn try_push_new_pass_on(of_setup_part: &str, on_results: &mut Vec<PassOn>) {
-    if of_setup_part.is_empty() {
-        return;
-    }
-    if of_setup_part.starts_with("$") {
-        if let Some(dot) = of_setup_part.find(".") {
-            let name = &of_setup_part[1..dot];
-            let what = &of_setup_part[dot + 1..];
-            if what == "all" {
-                on_results.push(PassOn::ExpectAllOf(String::from(name)));
-            } else if what == "each" {
-                on_results.push(PassOn::ExpectEachOf(String::from(name)));
-            } else {
-                let nth = what.parse::<u32>().expect("Could not parse Nth argument.");
-                on_results.push(PassOn::ExpectNthOf(nth, String::from(name)));
-            }
-        } else {
-            let name = &of_setup_part[1..];
-            on_results.push(PassOn::ExpectAllOf(String::from(name)));
-        }
-    } else {
-        on_results.push(PassOn::DirectLike(String::from(of_setup_part)));
-    }
 }
